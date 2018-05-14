d71741f458ca01e031c2a00aaad7659561502d7e
Christian Talmo <talmo.christian@gmail.com>
2018-05-14 13:52:40 +0300

    fixed default ( 6 aka 2015 ) ecmaVersion syntax checking for node versions above 6 by using acorn directly instead of syntax-error

--- passlint.js
+++ passlint.js
@@ -1,37 +1,44 @@
-var check = require( 'syntax-error' )
+var acorn = require( 'acorn' )
 
 module.exports = function ( text, ecmaVersion ) {
-  var err = check( text, '', {
-    ecmaVersion: ecmaVersion || 6 // 2015
-  } )
+  var options = {
+    ecmaVersion: ecmaVersion || 6
+  }
 
-  if ( err ) {
-    try {
-      /*
-       * Attempt to get a fuller error description
-       * ( usually the last line of err.toString() )
-       */
-      var string = String( err )
-      if ( string ) {
-        var split = string.split( '\n' )
-        split.forEach( function ( str ) {
-          var a = str.toLowerCase()
-          var b = err.message.toLowerCase()
-          if ( a.indexOf( b ) >= 0 ) {
-            err.message = str
-          }
-        } )
+  // ref: https://github.com/acornjs/acorn
+  try {
+    var ast = acorn.parse( text, options ) // eslint-disable-line
+  } catch ( err ) {
+    if ( err ) {
+      try {
+        /*
+        * Attempt to get a fuller error description
+        * ( usually the last line of err.toString() )
+        */
+        var string = String( err )
+        if ( string ) {
+          var split = string.split( '\n' )
+          split.forEach( function ( str ) {
+            var a = str.toLowerCase()
+            var b = err.message.toLowerCase()
+            if ( a.indexOf( b ) >= 0 ) {
+              err.message = str
+            }
+          } )
+        }
+      } catch ( err ) { /* ignore */ }
+
+      // add 'error' to the error message if it doesn't exist
+      if ( err.message.toLowerCase().indexOf( 'error' ) === -1 ) {
+        err.message = ( 'error: ' + err.message )
       }
-    } catch ( err ) { /* ignore */ }
 
-    // add 'error' to the error message if it doesn't exist
-    if ( err.message.toLowerCase().indexOf( 'error' ) === -1 ) {
-      err.message = ( 'error: ' + err.message )
+      var loc = err.loc
+
+      return (
+        loc.line + ':' + loc.column + ': ' + err.message
+      )
     }
-
-    return (
-      err.line + ':' + err.column + ': ' + err.message
-    )
   }
 
   return ''
