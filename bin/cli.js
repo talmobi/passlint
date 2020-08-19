5c9ec6d948b6bb7397d8068206e4608ddc417527
Christian Talmo <talmo.christian@gmail.com>
2020-08-20 02:22:46 +0300

    feat: use redstar to glob patterns

--- bin/cli.js
+++ bin/cli.js
@@ -6,15 +6,17 @@ var _fs = require( 'fs' )
 var _path = require( 'path' )
 var _pkg = require( '../package.json' )
 
+var _glob = require( 'redstar' )
+
 var argv = require( 'minimist' )( process.argv.slice( 2 ), {
   alias: {
-    'ecmaVersion': [ 'e', 'ev', 'ecma', 'ecmaversion', 'ecma-version' ],
-    'version': [ 'V' ],
-    'help': [ 'h' ]
+    ecmaVersion: [ 'e', 'ev', 'ecma', 'ecmaversion', 'ecma-version' ],
+    version: [ 'V' ],
+    help: [ 'h' ]
   }
 } )
 
-if ( argv[ 'help' ] ) {
+if ( argv.help ) {
   console.log(
     _fs.readFileSync(
       _path.join( __dirname, '../help.txt' ),
@@ -25,7 +27,7 @@ if ( argv[ 'help' ] ) {
   process.exit()
 }
 
-if ( argv[ 'version' ] ) {
+if ( argv.version ) {
   console.log(
     _pkg.name + ' version: ' + _pkg.version
   )
@@ -40,7 +42,7 @@ if ( argv._.length <= 0 ) {
 }
 
 var buffer = ''
-argv._.forEach( function ( file ) {
+function checkFile ( file ) {
   var errline
 
   try {
@@ -50,7 +52,7 @@ argv._.forEach( function ( file ) {
     if ( suffix.toLowerCase().indexOf( 'css' ) >= 0 ) {
       errline = _passlint( text, 'css' )
     } else {
-      errline = _passlint( text, argv[ 'ecmaVersion' ] )
+      errline = _passlint( text, argv.ecmaVersion )
     }
   } catch ( err ) {
     console.error( err.message.trim() )
@@ -61,12 +63,41 @@ argv._.forEach( function ( file ) {
     // TODO wooster piping working
     buffer += ( '  ' + file + ':' + errline + '\n' )
   }
+
+  finish.counter = finish.counter || 0 // init
+  finish.counter++ // increment
+
+  // check for completion
+  if ( finish.counter >= finish.endLimit ) {
+    finish()
+  }
+}
+
+finish.endLimit = finish.endLimit || 0 // init
+finish.endLimit += argv._.length // increment
+argv._.forEach( function ( file ) {
+  if ( _glob.hasMagic( file ) ) {
+    var pattern = file
+
+    _glob( pattern, function ( err, files, dirs ) {
+      finish.counter++
+
+      finish.endLimit += files.length
+      files.forEach( function ( file ) {
+        checkFile( file )
+      } )
+    } )
+  } else {
+    checkFile( file )
+  }
 } )
 
-process.stderr.write( buffer )
+function finish () {
+  process.stderr.write( buffer )
 
-if ( buffer === '' ) {
-  process.exit( 0 ) // no errors
-} else {
-  process.exit( 1 )
+  if ( buffer === '' ) {
+    process.exit( 0 ) // no errors
+  } else {
+    process.exit( 1 )
+  }
 }
